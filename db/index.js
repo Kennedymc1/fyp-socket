85b82144ac9f037f5cc63f26df6909603f516544
Kennedymc1 <125534338+Kennedymc1@users.noreply.github.com>
2023-09-19 21:05:35 +0200

    up date db connection
    update

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 exports.connect = () => {
-    const dbUrl = 'mongosh "mongodb+srv://kennedychanda:mwenyakc@cluster0.hipglub.mongodb.net/?retryWrites=true&w=majority" --apiVersion 1 --username kennedychanda';
+    const dbUrl = 'mongodb+srv://kennedychanda:mwenyakc@cluster0.hipglub.mongodb.net/?retryWrites=true&w=majority';
+
     mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
     mongoose.connection.on('error', (err) => console.log(err));
